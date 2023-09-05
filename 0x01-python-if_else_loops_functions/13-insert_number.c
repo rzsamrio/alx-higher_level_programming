7eadcc81774009aee012c59256a453c1086373a9
rzsamrio <samuelekop144@gmail.com>
2023-09-06 00:11:26 +0100

    edit gitignore to not exclude C files

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts ode in an ascending list
+ * @head: address of head node
+ * @number: number to insert
+ * Return: address of node, NULL if malloc fails
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *curr, *next, *new;
+
+	curr = *head;
+	new = malloc(sizeof(listint_t)); /* init node */
+	if (new == NULL)
+		return (NULL);
+	if (number < (*head)->n)	/* compare number with head */
+	{
+		new->n = number;
+		new->next = curr;
+		*head = new;
+		return (new);
+	}
+	while (curr != NULL)	/* loop through */
+	{
+		next = curr->next;
+		if (next != NULL)
+		{
+			if (number <= next->n && number >= curr->n)
+			{
+				new->n = number;
+				curr->next = new;
+				new->next = next;
+				break;
+			}
+			else	/* Iterate current and next pointers */
+			{
+				curr = next;
+				next = curr->next;
+			}
+		}
+		else	/* Once iteration ends, number must be > max number in list */
+		{
+			new->n = number;
+			curr->next = new;
+			new->next = NULL;
+			break;
+		}
+	}
+	return (new);
+}
+
