4db4a12f04823395fe8724d7cbd6cc9bc1d2ff66
rzsamrio <samuelekop144@gmail.com>
2023-09-08 05:41:05 +0100

    main handling

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-j = -1
-for x in sys.argv:
-    j += 1
-    if j == 0:
-        continue
-    sum += int(x)
-print("{:d}".format(sum))
+if __name__ == '__main__':
+    import sys
+    msum = 0
+    j = -1
+    for x in sys.argv:
+        j += 1
+        if j == 0:
+            continue
+        msum += int(x)
+    print("{:d}".format(msum))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-names = [name for name in dir(hidden_4) if name[:2] != "__"]
-names.sort()
-for i in names:
-    print(i)
+if __name__ == '__main__':
+    import hidden_4
+    names = [name for name in dir(hidden_4) if name[:2] != "__"]
+    names.sort()
+    for i in names:
+        print(i)
