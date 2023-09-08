b7baa93dcd793fbd5b220a35619ae331a4be5298
rzsamrio <samuelekop144@gmail.com>
2023-09-08 05:52:06 +0100

    Bug fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ def main():
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
 
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,13 @@ if __name__ == '__main__':
     arg_len = len(sys.argv) - 1
     if arg_len == 0:
         print("{:d} arguments.".format(arg_len))
+    elif arg_len == 1:
+        print("{:d} argument:".format(arg_len))
     else:
         print("{:d} arguments:".format(arg_len))
-        j = -1
-        for arg in sys.argv:
-            j += 1
-            if j == 0:
-                continue
-            print("{:d}: {}".format(j, arg))
+    j = -1
+    for arg in sys.argv:
+        j += 1
+        if j == 0:
+            continue
+        print("{:d}: {}".format(j, arg))
