d388cdf16cccd51eeb378206abebc3e9f7ff93cc
rzsamrio <samuelekop144@gmail.com>
2023-09-12 13:40:36 +0100

    pycheck + task 5 fix: multi c compatible

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,12 @@
 def no_c(my_string):
     """Deletes the cs in a string"""
     string = list(my_string)
-    for i, a in enumerate(string):
-        if a == 'c' or a == 'C':
-            del(string[i])
+    for i, chr in enumerate(string):
+        if chr == 'C':
+            string[i] = 'c'
+    lnum = string.count('c')  # loop num
+    while lnum:
+        index = string.index('c')
+        del(string[index])
+        lnum -= 1
     return "".join(string)
