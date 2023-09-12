85f3d362ce663dc028ceb7d23685e69abf055886
rzsamrio <samuelekop144@gmail.com>
2023-09-12 05:50:09 +0100

    Account for empty sequence

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Prints a list in reverse"""
+    if len(my_list) == 0:
+        return
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
-
-
-if __name__ == '__main__':
-    print_reversed_list_integer()
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return
     string = list(my_string)
     for i, a in enumerate(string):
         if a == 'c' or a == 'C':
