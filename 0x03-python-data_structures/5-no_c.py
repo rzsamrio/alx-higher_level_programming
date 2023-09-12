b243f6718546bc7e8136c2534e1de2963957c449
rzsamrio <samuelekop144@gmail.com>
2023-09-12 05:59:37 +0100

    reset 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if len(my_string) == 0:
-        return
+    """Deletes the cs in a string"""
     string = list(my_string)
     for i, a in enumerate(string):
         if a == 'c' or a == 'C':
