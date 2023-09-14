2711d1dc30cb58f70c5fbb90f99a881bfd7bc73e
rzsamrio <samuelekop144@gmail.com>
2023-09-14 05:52:18 +0100

    bug fix: Account for empty dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if a_dictionary == {}:
+        return None
     return max(a_dictionary, key=lambda k: a_dictionary[k])
