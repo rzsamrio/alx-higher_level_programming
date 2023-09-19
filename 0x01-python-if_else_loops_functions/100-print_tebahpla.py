6d202bc9f09c93b5702b5e77118266a147abb9e9
rzsamrio <samuelekop144@gmail.com>
2023-09-19 03:46:41 +0100

    add task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/bin/python3
-for i in range(ord('z'), ord('a'), -1):
-	if i % 2 != 0:
-		print("{}"chr(i).upper(), end="")
-	else:
-		print(chr(i), end="")
-print()
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 != 0:
+        z = i - 32
+    else:
+        z = i
+    print("{}".format(chr(z)), end="")
