b8b2d5c985dcd538b17b6078122355d32dde003b
rzsamrio <samuelekop144@gmail.com>
2023-09-26 05:34:13 +0100

    0: use counter instead

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    j = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
+            j += 1
         except IndexError:
             print()
-            return (i)
+            return (j)
     print()
-    return (i + 1)
+    return (j)
