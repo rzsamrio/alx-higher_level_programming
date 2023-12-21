44b6af5e138c6f06aaa9414ea608e9cb6f6e9516
rzsamrio <samuelekop144@gmail.com>
2023-12-21 04:34:01 +0100

    restore 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,35 @@
 #include "lists.h"
 
-/**
- * check_cycle - checks for a cycle in the singly linked list
- * @list: list to check
- * Return: 1 if cycle and 0 if not
- */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp, *head = list;
+	int i, j, len;
+	listint_t **addr, **tmp, *current = list;
 
-	if (list == NULL)
-		return (0)
-
-	tmp = head->next;
-	while (tmp != NULL)
+	len = 10;
+	addr = malloc(sizeof(listint_t *) * len);
+	for (i = 0; current != NULL; i++)
 	{
-		if (tmp == head)
-			return (1);
-		tmp = tmp->next;
+		if (i == len) /* Realloc? */
+		{
+			len += 10;
+			tmp = malloc(sizeof(listint_t *) * len);
+			for (j = 0; j < (len - 10); j++)
+				tmp[j] = addr[j];
+			free(addr);
+			addr = tmp;
+		}
+
+		for (j = 0; j < i; j++) /* Check the saved address list for a match */
+		{
+			if (current == addr[j])
+			{
+				free(addr);
+				return (1);
+			}
+		}
+		addr[i] = current;
+		current = current->next;
 	}
-	return (0);
+	free(addr);
+	return (0); /* Program is O(n^2) */
 }
