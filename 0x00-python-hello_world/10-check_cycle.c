93f9b2afe9340de069a1e4f4035c9436d67308aa
rzsamrio <samuelekop144@gmail.com>
2023-12-21 11:10:01 +0100

    10 fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,7 +13,7 @@ int check_cycle(listint_t *list)
 		return (0);
 
 	tmp = list->next;
-	if (tmp->next == NULL)
+	if (tmp == NULL || tmp->next == NULL)
 		return (0);
 	s2 = tmp->next->next;
 	while (s2 != NULL || tmp != NULL)
