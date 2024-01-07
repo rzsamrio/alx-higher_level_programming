3e828bae628b052b26edab6d9bfd6b442c5d74aa
rzsamrio <samuelekop144@gmail.com>
2024-01-07 21:59:59 +0100

    Add unittesst

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class Testfunction(unittest.TestCase):
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer("Str"))
+        self.assertRaises
+    def test_val(self):
+        self.assertAlmostEqual(max_integer([1, 5, 9, 24, 2, 6]), 24)
+        self.assertAlmostEqual(max_integer([-1, -7, -11]), -1)
+        self.assertAlmostEqual(max_integer([]), None)
