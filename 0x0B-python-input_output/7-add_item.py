ae27136b4d2fc124f92a786377be9d6a6d66a21f
rzsamrio <samuelekop144@gmail.com>
2024-01-10 03:55:35 +0100

    remove all with-open statements

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,14 +9,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    with open("add_items.json", 'r') as f:
-        pass
+    json_data = load_from_json_file("add_items.json")
 except FileNotFoundError:
-    with open("add_items.json", 'w') as f:
-        f.write("[]")
+    json_data = []
 
 argc = len(sys.argv)
-json_data = load_from_json_file("add_items.json")
 for i in range(1, argc):
     json_data.append(sys.argv[i])
 save_to_json_file(json_data, "add_items.json")
