3566e5d310a66330484603c68a1044fc8430cca8
rzsamrio <samuelekop144@gmail.com>
2024-01-10 14:30:55 +0100

    add staticmethod

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,8 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    @staticmethod
+    def integer_validator(name, value):
         """ Validate an integer """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
