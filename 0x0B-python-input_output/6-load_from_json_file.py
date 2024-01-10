382e94aae6c8c3431fd06a064f2e706f9d711839
rzsamrio <samuelekop144@gmail.com>
2024-01-10 03:17:27 +0100

    add task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr//bin/python3
+""" Contains the load_from_json_file function """
+
+
+def load_from_json_file(filename):
+    """ Returns the object representation of a json string
+    From a file
+
+    Args:
+        filename (str): Name of file to read json data
+
+    Return: Python object """
+
+    import json
+    with open(filename, 'r', encoding='utf-8') as f:
+        json_obj = json.load(f)
+    return json_obj
