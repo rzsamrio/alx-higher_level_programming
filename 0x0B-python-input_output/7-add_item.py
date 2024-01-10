765685c9b0f134bea5fc4edc1b17bbbc7e0d0b20
rzsamrio <samuelekop144@gmail.com>
2024-01-10 04:03:58 +0100

    add-item not add-items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    json_data = load_from_json_file("add_items.json")
+    json_data = load_from_json_file("add_item.json")
 except FileNotFoundError:
     json_data = []
 
 argc = len(sys.argv)
 for i in range(1, argc):
     json_data.append(sys.argv[i])
-save_to_json_file(json_data, "add_items.json")
+save_to_json_file(json_data, "add_item.json")
