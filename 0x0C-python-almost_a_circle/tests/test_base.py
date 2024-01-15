512b18e04ce185a1138ab1b78c631106beaf2c9b
rzsamrio <samuelekop144@gmail.com>
2024-01-15 23:43:12 +0100

    last attempt

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,13 +3,11 @@
 
 
 import unittest
-from sys import maxsize
 from models.base import Base
 
 class TestBase(unittest.TestCase):
 	def testid(self):
 		self.assertEqual(Base(94).id, 94)
-		self.assertEqual(Base(maxsize).id, maxsize)
 		self.assertEqual(Base().id, 1)
 		self.assertEqual(Base().id, 2)
 		self.assertEqual(Base(175).id, 175)
