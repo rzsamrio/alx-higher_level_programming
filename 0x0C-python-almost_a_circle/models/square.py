da29c527df1e3a72f12df9d3e912804b87f268e4
rzsamrio <samuelekop144@gmail.com>
2024-01-28 03:31:40 +0100

    suit square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,13 +32,11 @@ class Square(Rectangle):
         if len(args) > 0:
             self.id = args[0]
         if len(args) > 1:
-            self.width = args[1]
+            self.size = args[1]
         if len(args) > 2:
-            self.height = args[2]
+            self.x = args[2]
         if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+            self.y = args[3]
 
     def to_dictionary(self):
         """ Return a dictionary of Instance properties """
