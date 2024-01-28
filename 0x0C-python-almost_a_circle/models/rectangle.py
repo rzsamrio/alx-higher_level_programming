2dca3ca3118b264fe244b4defc5af1c9b8ab7618
rzsamrio <samuelekop144@gmail.com>
2024-01-28 03:19:55 +0100

    tired

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,20 +49,16 @@ class Rectangle(Base):
                 if key in ["id", "width", "height", "x", "y"]:
                     setattr(self, key, kwargs[key])
             return
-        for i, arg in enumerate(args):
-            match i:
-                case 0:
-                    self.id = arg
-                case 1:
-                    self.width = arg
-                case 2:
-                    self.height = arg
-                case 3:
-                    self.x = arg
-                case 4:
-                    self.y = arg
-                case _:
-                    pass
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
 
     def to_dictionary(self):
         """ Returns a dictionary of Instance's properties """
