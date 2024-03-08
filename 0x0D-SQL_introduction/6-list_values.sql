6a3d1203de83cfef24495eccb8c0fb3421692dea
rzsamrio <samuelekop144@gmail.com>
2024-03-08 03:32:16 +0100

    [0x0D] Add task 5..7

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Prints the contents of a table
+-- Analogous to listing all the rows f a table
+SELECT *
+FROM first_table
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert a new row into table
+INSERT INTO first_table
+VALUES (89, 'Best School');
