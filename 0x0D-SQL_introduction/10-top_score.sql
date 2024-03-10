086597f2df11a749b24e7458f4e5e7689044eb01
rzsamrio <samuelekop144@gmail.com>
2024-03-10 08:46:32 +0100

    [0x0D] Add 8..10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- List specific records in second_table
+SELECT `score`, `name`
+FROM second_table
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Counts the number of recods with an ID 79
+SELECT COUNT(*)
+FROM first_table
+WHERE id = 89
