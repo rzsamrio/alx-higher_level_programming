afa0ce7ce25ab9a7f4b7c46777ec45976350f73c
rzsamrio <samuelekop144@gmail.com>
2024-03-13 03:53:35 +0100

    [0x13] Add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return null;
+    } else if (w === undefined || h === undefined) {
+      return null;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let res = 'X';
+      for (let j = 1; j < this.width; j++) {
+        res += 'X';
+      }
+      console.log(res);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
