a3a08d11348ea65808b2ac2ffd6e1c85ca61e9c2
rzsamrio <samuelekop144@gmail.com>
2024-04-09 11:49:47 +0100

    [0x0D] Fix15: Order condition

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- Display the number of records with the same score
-SELECT `score`, COUNT(*) as `number` FROM `second_table`
-GROUP BY `score`;
+SELECT `score`, COUNT(score) as `number` FROM `second_table`
+GROUP BY `score`
+ORDER BY `score` DESC;
