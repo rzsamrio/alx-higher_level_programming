409dc3c6cc3533991ab981076b8e3aa73a706e09
rzsamrio <samuelekop144@gmail.com>
2024-04-10 17:11:26 +0100

    [0X0E] Add task 11 and 12

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- List all shows and genre id from a group of relations
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC; 
---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- List all shows and genre id from a group of relations
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC; 
