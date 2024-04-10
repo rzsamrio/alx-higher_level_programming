a7cae8e9d4168d705c919fb6f2f23b237c93e263
rzsamrio <samuelekop144@gmail.com>
2024-04-10 18:30:48 +0100

    [0X0E] Fix14: No hardcoding

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- Lists all genres of show, Dexter
 SELECT name FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id = 8
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
