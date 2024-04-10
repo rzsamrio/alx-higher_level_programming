2ebf85b0744e546b109f37ee7e42f334aa51ebb6
rzsamrio <samuelekop144@gmail.com>
2024-04-10 08:16:58 +0100

    [0X0E] Add 2..4

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates a database and user
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
