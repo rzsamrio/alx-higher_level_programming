88dca5f626d674a50a6dbf70320805cabdbbf96e
rzsamrio <samuelekop144@gmail.com>
2024-04-28 10:58:25 +0100

    [0x13] reset size to h, w

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,16 +4,15 @@ const SquareAlpha = require('./5-square');
 class Square extends SquareAlpha {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       let out = c;
-      for (let j = 1; j < this.size; j++) {
+      for (let j = 1; j < this.width; j++) {
         out += c;
       }
       console.log(out);
