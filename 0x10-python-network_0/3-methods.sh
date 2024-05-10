f46b98426892356394c67e2cfb8c1f02e0eab309
rzsamrio <samuelekop144@gmail.com>
2024-05-10 08:43:39 +0100

    [PY:0x10] syntax error

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # View all method allowed by the URI (-i includes the http response header in the code)
-curl -i -sX  OPTIONS "$1" | grep 'Allow' | cut -d : f2 | cut -b 2-
+curl -i -sX  OPTIONS "$1" | grep 'Allow' | cut -d : -f2 | cut -b 2-
