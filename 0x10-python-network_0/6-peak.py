31bd05fbb0c8a492f8698085a61ab1cc1e9b3f6c
rzsamrio <samuelekop144@gmail.com>
2024-05-11 12:11:22 +0100

    pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Technical interview prep: 
-	Finding the peak value in a list """
+""" Technical interview prep:
+    Finding the peak value in a list """
 
 
 def find_peak(list_of_integers=[]):
-	""" Function that finds the peak value in a list """
-	list_of_integers.sort()
-	try:
-		return (list_of_integers[-1])
-	except IndexError:
-		return None
+    """ Function that finds the peak value in a list """
+    list_of_integers.sort()
+    try:
+        return (list_of_integers[-1])
+    except IndexError:
+        return None
