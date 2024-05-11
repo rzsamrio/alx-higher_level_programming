95609f8ba61cb311994f219040108b563dca1635
rzsamrio <samuelekop144@gmail.com>
2024-05-11 15:02:02 +0100

    [0x11] REAL: edit task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
-!/usr/bin/python3
-# Get the content of a url
-# curl equiv: `curl <url>` but with formatting
-
+#!/usr/bin/python3
+""" Get the content of a url
+    curl equiv: `curl <url>` but with formatting """
 import urllib.request
+
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as url:
     body = url.read()
 print("Body response:")
