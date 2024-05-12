df9940b69b6dc71aecb7b2ac9029a24cfedd1d8d
rzsamrio <samuelekop144@gmail.com>
2024-05-12 21:14:17 +0100

    [0x11] add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Prints the Header id value of a request """
+import requests
+import sys
+
+if __name__ == '__main__':
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
