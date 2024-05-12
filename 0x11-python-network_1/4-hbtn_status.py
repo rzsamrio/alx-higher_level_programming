cdf13118a835b64dfe9052e10c147fa991336bc0
rzsamrio <samuelekop144@gmail.com>
2024-05-12 20:54:47 +0100

    [0x11] fix comment positioning

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import requests
 """ Getting web content with the requests model """
+import requests
 
 if __name__ == '__main__':
     req = requests.get("https://alx-intranet.hbtn.io/status")
