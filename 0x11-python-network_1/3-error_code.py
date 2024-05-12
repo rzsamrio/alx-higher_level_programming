1703fb93a3ef00a8368a975009ceb0ade8d56fd7
rzsamrio <samuelekop144@gmail.com>
2024-05-12 19:55:46 +0100

    [0x11] testrun add task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Handle error code with GET requests """
+from sys import argv
+from urllib.error import HTTPError, URLError
+import urllib.reqest
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(argv[1]) as resp:
+            body = resp.read()
+        print(body.decode())
+    except HTTPError as err:
+        print(f"Error code: {err.code}")
+    except URLError as err:
+        print(err.reason)
